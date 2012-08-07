eaab78418a756beaff7fccb606c38b47c33f4161
adchilds <adchilds@eckerd.edu>
2012-08-07 13:08:17 -0400

    Comments and descriptions changed/fixed.

--- src/plugins/butterworth.py
+++ src/plugins/butterworth.py
@@ -4,7 +4,7 @@ import numpy as np
 import wx
 
 class Filters(IPlugin):
-    """ Fast Fourier Transform + Butterworth Highpass filter """
+    """ Butterworth Highpass filter """
 
     def __init__(self):
         pass
@@ -23,29 +23,29 @@ class Filters(IPlugin):
     def calc_filter(self):
         """ Must provide an implementation for the run method """
 
-        wx.CallAfter(self.pb.update, 'Applying FFT + Butterworth HPF to overlay ' + str(self.overlay_num))
+        wx.CallAfter(self.pb.update, 'Applying Butterworth HPF to overlay ' + str(self.overlay_num))
         iht, iwd = self.coral_slab.shape
-        #zero pad image to a power of 2 to speed up FFT
+        # zero pad image to a power of 2 to speed up FFT
         vpad = 2**(int(math.ceil(math.log(iht, 2))))
         hpad = 2**(int(math.ceil(math.log(iwd, 2))))
         fImg = np.zeros(shape=(vpad, hpad), dtype=np.double, order='C')
         fImg[0:iht, 0:iwd] = self.coral_slab
-        #FFT
+        # FFT
         fImg = np.fft.fftshift(np.fft.fft2(fImg))
-        #find center row and column
+        # find center row and column
         cr = fImg.shape[0]/2
         cc = fImg.shape[1]/2
-        #compute distance for every pixel from center
+        # compute distance for every pixel from center
         D = np.ones(shape=(vpad, hpad), dtype=np.double, order='C')
         for row in range(iht):
             for col in range(iwd):
                 D[row][col] = math.sqrt((col-cc)**2 + (row-cr)**2)
         D[cr][cc] = 0.00000001  # avoid zero-division warning 
-        #Create the Butterworth, high-pass filter
+        # Create the Butterworth, high-pass filter
         Do = 25
         p = 2
         H = 1/(1 + (Do/D)**(2*p))
-        #Highpass filter the source image & strip off the zero-padded regions
+        # Highpass filter the source image & strip off the zero-padded regions
         fi = H*fImg
         fi = np.fft.ifftshift(fi)
         fi = np.abs(np.fft.ifft2(fi))
@@ -55,4 +55,4 @@ class Filters(IPlugin):
         self.overlay_controller.overlays.append(self.coral_slab - fi)
         self.overlay_controller.alphas.append(0)
         
-        wx.CallAfter(self.pb.update, 'Completed FFT + Butterworth Highpass Filter')
+        wx.CallAfter(self.pb.update, 'Completed Butterworth Highpass Filter')

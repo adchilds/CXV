54262ec48e9be8a35c5b80f3470c918ace60d847
adchilds <adchilds@eckerd.edu>
2013-01-28 17:48:59 -0500

    Removed some whitespace

--- src/lib/progress_bar.py
+++ src/lib/progress_bar.py
@@ -12,6 +12,7 @@ import wx
 
 class ProgressBar(wx.ProgressDialog):
     """Simple sub class of wx.ProgressDialog for various modules to use"""
+    
     def __init__(self, title, first, max, parent):
         wx.ProgressDialog.__init__(self, title, first, max, parent, style=wx.PD_AUTO_HIDE)
         self.counter = 1

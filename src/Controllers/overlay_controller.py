Rotation fixed. Should be working 100% with overlays.

src/Controllers/overlay_controller.py
@@ -17,22 +17,24 @@ from lib import progress_bar
 from Views import overlay_view
 from yapsy.PluginManager import PluginManager
 import numpy as np
-import math
 import os
 import re
 import wx
 
 class Controller():
     
-    def __init__(self, dicom_view, dicom_controller, model, background, show, rotations=0):
+    def __init__(self, dicom_view, dicom_controller, model, background, show, rotations=0, alphas=None):
         self.dicom_view = dicom_view
         self.dicom_controller = dicom_controller
         self.model = model
         self.patt = re.compile('\d+')
         self.overlay = 0.0
         self.overlays = []
-        self.alphas = []
-        self.view = overlay_view.View(self, self.dicom_view)
+        if alphas is not None:
+            self.alphas = alphas
+        else:
+            self.alphas = []
+        self.view = overlay_view.View(self, self.dicom_view, alphas=alphas)
         self.background = background
         self.show = show
         self.rotations = rotations
@@ -41,6 +43,7 @@ class Controller():
         # Rotate each filter
         for i in xrange(len(self.overlays)):
             np.rot90(self.overlays[i]) # Rotate the region
+        np.rot90(self.overlay)
 
     def getPluginCount(self):
         # Get the default plugin directory, using XML
@@ -130,7 +133,7 @@ class Controller():
         elif self.alphas[len(self.alphas) - 1] > 100:
             self.alphas[len(self.alphas) - 1] = 100
 
-    def create_overlays(self):
+    def create_overlays(self, alphas=None):
         """ Initializes the progress bar.
         
         Sets the total number of progress bar units to the total
@@ -141,7 +144,10 @@ class Controller():
         Initializes the plugin controller, which then runs the plugin algorithms.
         """
         pb = progress_bar.ProgressBar('Creating Overlays', 'Locking coral region', ((self.getPluginCount() * 2) + 1), self.dicom_view)
-        plugin_controller.Controller(pb, self, self.dicom_controller, self.model, self.rotations)
+        plugin_controller.Controller(pb, self, self.dicom_controller, self.model, self.rotations, alphas=alphas)
+        
+        if alphas is not None:
+            self.alphas = alphas
 
     def remove_overlays(self):
         self.dicom_view.figure.delaxes(self.dicom_view.ov_axes)
@@ -150,7 +156,8 @@ class Controller():
         x, y, dx, dy = self.dicom_controller.coral_slab
         iH, iW = self.model.get_image_shape()
 
-        # Swap x and y if image is rotated
+        # Swap x and y if image is rotated to correctly
+        # position the filter (otherwise wont be inside bbox)
         if self.dicom_controller.rotations == 1 or self.dicom_controller.rotations == 3:
             temp = iH
             iH = iW
@@ -178,7 +185,7 @@ class Controller():
         """
         # Use linear combination for displaying overlay
         self.overlay = 0.0 # Reset to 0.0; if we don't the new overlays are appended to the old!
-        if not alphas:
+        if alphas is None:
             for ov in range(len(self.overlays)):
                 self.overlay += (self.alphas[ov]/100.0) * self.overlays[ov]
         else:
@@ -193,7 +200,10 @@ class Controller():
             x = dx
             y = dy
         else:
-            y, x = self.overlay.shape
+            try:
+                y, x = self.overlay.shape
+            except AttributeError:
+                return
 
         rgba, ptr = self.model.allocate_array((y, x, 4))
         rgba = self.model.set_display_data(rgba, self.overlay, 1.0)

src/Controllers/plugin_controller.py
@@ -24,18 +24,24 @@ class Controller(threading.Thread):
     loading of the files, the running of the scripts, etc.
     """
 
-    def __init__(self, pb, overlay_controller, dicom_controller, model, rotations):
+    def __init__(self, pb, overlay_controller, dicom_controller, model, rotations, alphas=None):
         threading.Thread.__init__(self)
         self.pb = pb # progress bar
         self.controller = overlay_controller
         self.dicom_controller = dicom_controller
         self.model = model
         self.rotations = rotations
+        self.alphas = alphas
         self.start()
 
     def run(self):
         """ Runs the given algorithms """
-        wx.CallAfter(self.pb.update, 'Retrieving coral region')
+        self.controller.overlays = []
+        if self.alphas is None:
+            self.controller.alphas = []
+
+        if self.pb is not None:
+            wx.CallAfter(self.pb.update, 'Retrieving coral region')
 
         # Load in original dicom pixel data and normalize
         coral_slab = self.model.ds.pixel_array.astype(np.double)
@@ -71,7 +77,7 @@ class Controller(threading.Thread):
         count = 0;
         for plugin in manager.getAllPlugins():
             # Initialize the plugin so that we can call it's methods
-            plugin.plugin_object.initPlugin(self.controller, coral_slab, self.model, self.pb, count)
+            plugin.plugin_object.initPlugin(self.controller, coral_slab, self.model, self.pb, count, self.alphas)
 
             # Run the plugin's algorithm
             plugin.plugin_object.calc_filter()
@@ -81,15 +87,22 @@ class Controller(threading.Thread):
 
         # Add the original coral_slab to the overlay
         self.controller.overlays.append(coral_slab)
-        self.controller.alphas.append(100)
+
+        if self.alphas is None:
+            self.controller.alphas.append(100)
+        else:
+            self.controller.alphas = []
+            self.controller.alphas = self.alphas
+
         wx.CallAfter(self.controller.add_overlay)
 
         # Update the progress bar to let the user know we've finished
         # running the algorithms
-        wx.CallAfter(self.pb.finish, 'Finishing up')
+        if self.pb is not None:
+            wx.CallAfter(self.pb.finish, 'Finishing up')
 
         # Display the filters
-        if self.controller.show:
+        if self.controller.show and self.alphas is None:
             # Get the size of alphas (number of filters) minus one.
             # The last alpha should be 0. Set all the alphas to the same
             # value (except for the last one), so divide 100 by the number